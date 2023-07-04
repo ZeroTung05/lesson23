f5351a2bafbb3c111737d3245da6d7870e3eeb96
ZeroTung05 <s.ch2605@mail.ru>
2023-07-05 02:05:55 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
         System.out.println(sb.toString());
         sb.append(" my").append(" friend");
         System.out.println(sb.toString());
+        System.out.printf("String %10d \n",512);
+        System.out.printf("String %10d \n",12331222);
+        System.out.printf("String %10d \n",51231212);
+        System.out.printf("String %10d \n",511232112);
+        System.out.printf("String %.2f \n",51.1232112);
     }
 }
